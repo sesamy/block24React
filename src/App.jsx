0995a81304ae84c1caa57389eddfb72587cb4b87
sesamy <amy.hori97@gmail.com>
2023-11-02 16:04:25 -0700

    passing data, hooking to work with updates, making initial map list

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,17 @@
+import { useState } from "react";
 import "./App.css";
+import { puppyList } from "./data.js";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log("puppyList:", puppyList);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card"></div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      {puppies.map((puppy) => {
+        return <p key={puppy.id}>{puppy.name}</p>;
+      })}
+    </div>
   );
 }
 
