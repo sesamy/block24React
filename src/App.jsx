0c3e67502c6ffb91be3fac85680dd535e067d291
sesamy <amy.hori97@gmail.com>
2023-11-02 17:15:05 -0700

    puppy details rendering on page

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,20 @@ function App() {
   const [featPupId, setFeatPupId] = useState(null);
   console.log("puppyList:", puppyList);
 
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
+
   return (
     <div className="App">
-      {featPupId && <p>{featPupId}</p>}
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
       {puppies.map((puppy) => {
         return (
           <p
